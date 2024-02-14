0b1a00d2b7fd01fe61dcd08926ececef8b15dadd
kmangaty <kmaangaty@gmail.com>
2024-02-14 20:53:14 +0200

    10

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,14 @@
--- prepares a MySQL server for the project
-
+-- Create or use the database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or use the user hbnb_dev with the password hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Show grants for hbnb_dev
+SHOW GRANTS FOR 'hbnb_dev'@'localhost';

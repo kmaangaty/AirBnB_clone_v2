969996eadeb8b983bf623cb61feb486e8250edd7
kmangaty <kmaangaty@gmail.com>
2024-02-14 22:18:51 +0200

    19

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,65 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script that distributes an archive to web servers
 """
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+from datetime import datetime
+from fabric.api import env, put, run, local
+import os
+
+env.user = 'ubuntu'
+env.hosts = ['100.27.0.247', '100.26.162.119']
+
+
+def do_pack():
+    """
+    Generate a .tgz archive from web_static
+
+    Returns:
+        str: Archive path if generated successfully, None otherwise
+    """
+    try:
+        d = datetime.now().strftime("%Y%m%d%H%M%S")
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        fn = "versions/web_static_{}.tgz".format(d)
+        local("tar -cvzf {} web_static".format(fn))
+        return fn
+    except ValueError:
+        return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+    Distribute an archive to web servers
+
+    Args:
+        archive_path (str): Path to the archive file
+
+    Returns:
+        bool: True if deployment successful, False otherwise
+    """
+    if not os.path.exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        # Upload archive
+        put(archive_path, "/tmp/")
+        # Extract archive to releases folder
+        archive_filename = os.path.basename(archive_path)
+        release_path = "/data/web_static/releases/{}".format(
+            archive_filename[:-4])
+        run("mkdir -p {}".format(release_path))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_path))
+        # Move files out of web_static folder
+        run("mv {}/web_static/* {}".format(release_path, release_path))
+        # Remove unnecessary web_static folder
+        run("rm -rf {}/web_static".format(release_path))
+        # Remove previous symbolic link
+        run("rm -rf /data/web_static/current")
+        # Create new symbolic link
+        run("ln -s {} /data/web_static/current".format(release_path))
+        print("New version deployed!")
         return True
-    except:
+    except ValueError:
         return False

c05cde6908c940661ad006e82097a26d72a4e8fe
kmangaty <kmaangaty@gmail.com>
2024-02-14 20:59:32 +0200

    10

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,14 @@
--- prepares a MySQL server for the project
-
+-- Create or use the database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create or use the user hbnb_test with the password hbnb_test_pwd
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Show grants for hbnb_test
+SHOW GRANTS FOR 'hbnb_test'@'localhost';

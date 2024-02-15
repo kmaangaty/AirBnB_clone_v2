2b7f25de2e637951a0943f7850815550c821ef3f
kmangaty <kmaangaty@gmail.com>
2024-02-15 22:18:37 +0200

    24

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,31 @@
-#!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
-
 from fabric.api import put, run, env
 from os.path import exists
+
 env.hosts = ['100.27.0.247', '100.26.162.119']
 
 
 def do_deploy(archive_path):
-    """ deploy web """
+    """ Deploy web """
     if exists(archive_path) is False:
         return False
+
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
+
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('mkdir -p {}/{}'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}/{}'.format(file_n, path, no_ext))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+
+        # Move files directly instead of entire web_static directory
+        run('mv {0}/{1}/web_static/* {0}/{1}/'.format(path, no_ext))
+        run('rm -rf {}/web_static'.format(path + no_ext))
+
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {0}/{1}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except ValueError:
+    except Exception as e:
+        print("Error:", e)
         return False

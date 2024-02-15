145e183a24dce390136be78e3110f492ff753de3
kmangaty <kmaangaty@gmail.com>
2024-02-15 22:13:08 +0200

    22

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,7 +10,7 @@ env.hosts = ['100.27.0.247', '100.26.162.119']
 
 
 def do_deploy(archive_path):
-    """ deploy web """
+    """ Deploy web """
     if exists(archive_path) is False:
         return False
     try:
@@ -18,13 +18,14 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('mkdir -p {}/{}'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}/{}'.format(file_n, path, no_ext))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mv {0}/{1}/web_static/* {0}/{1}/'.format(path, no_ext))
+        run('rm -rf {}/web_static'.format(path + no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {0}/{1}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except ValueError:
+    except Exception as e:
+        print("Error:", e)
         return False
